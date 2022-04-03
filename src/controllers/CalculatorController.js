bf96f182635b774cc1e150429954967de2dffb4e
Adel <adel.cabrera.mesina@gmail.com>
2022-04-03 18:35:09 -0400

    ADD: links

--- src/controllers/CalculatorController.js
+++ src/controllers/CalculatorController.js
@@ -8,19 +8,13 @@ class CalculatorController {
   }
 
   calcularInteres(tasa, capital, anios) {
-    console.log("capital", capital);
-    console.log("tasa", tasa);
-    console.log("anios", anios);
-
     return capital * (tasa / 100) * anios;
   }
 
   renderResponse(req, res) {
     const { tasa, capital, anios } = req.body;
-    console.log(tasa, capital, anios);
 
     const total = this.calcularInteres(tasa, capital, anios);
-    console.log("total", total);
 
     res.render("interes", {
       tasa,
